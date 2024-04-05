9320cdbefec9a2b06a2c452bb68f857b2fdf3422
LucasOliveiraaq <lucasp.oliveira122@gmail.com>
2024-04-05 20:31:27 -0300

    Create event

--- src/main/java/lucaspo/com/passin/repositories/AttendeeRepository.java
+++ src/main/java/lucaspo/com/passin/repositories/AttendeeRepository.java
@@ -3,5 +3,9 @@ package lucaspo.com.passin.repositories;
 import lucaspo.com.passin.domain.attendee.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+
+    List<Attendee> findByEventId(String eventId);
 }
--- src/main/java/lucaspo/com/passin/services/EventService.java
+++ src/main/java/lucaspo/com/passin/services/EventService.java
@@ -1,19 +1,44 @@
 package lucaspo.com.passin.services;
 
 import lombok.RequiredArgsConstructor;
+import lucaspo.com.passin.domain.attendee.Attendee;
 import lucaspo.com.passin.domain.event.Event;
+import lucaspo.com.passin.dto.event.EventRequestDTO;
 import lucaspo.com.passin.dto.event.EventResponseDTO;
+import lucaspo.com.passin.repositories.AttendeeRepository;
 import lucaspo.com.passin.repositories.EventRepository;
 import org.springframework.stereotype.Service;
+
+import java.text.Normalizer;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EventService {
 
     private final EventRepository eventRepository;
+    private final AttendeeRepository attendeeRepository;
 
     public EventResponseDTO getEventDetail(String eventId) {
         Event event = this.eventRepository.findById(eventId).orElseThrow(() -> new RuntimeException("Event not found with ID: " + eventId));
-        return new EventResponseDTO(event, 0);
+        List<Attendee> attendeeList = this.attendeeRepository.findByEventId(eventId);
+        return new EventResponseDTO(event, attendeeList.size());
+    }
+
+    public void createEvent(EventRequestDTO eventDTO) {
+        Event newEvent = new Event();
+        newEvent.setTitle(eventDTO.title());
+        newEvent.setDetails(eventDTO.details());
+        newEvent.setMaximunAttendees(eventDTO.maximunAttendees());
+        newEvent.setSlug(this.createSlug(eventDTO.title()));
+        this.eventRepository.save(newEvent);
+    }
+
+    private String createSlug(String text) {
+        String normalized = Normalizer.normalize(text, Normalizer.Form.NFD);
+        return normalized.replaceAll("[\\p{InCOMBINING_DIACRITICAL_MARKS}]", "")
+                .replaceAll("[^\\w\\s]", "")
+                .replaceAll("\\s+", "-")
+                .toLowerCase();
     }
 }
